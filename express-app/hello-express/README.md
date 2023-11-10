7f2251075e89dcba7154f6c9a271c1e9c592b43b
Sam Vitare <sandhya.palanki@gmail.com>
2023-11-09 20:01:35 -0600

    added more new code

---
+++ express-app/hello-express/README.md
@@ -0,0 +1,9 @@
+# First express app
+
+Run `node index.js` command from the project root to run the app.
+
+## Practice code:
+
+[2.1 Server-side with Node.js - Working with Data and APIs in JavaScript](https://www.youtube.com/watch?v=wxbQP1LMZsw&list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X&index=10)
+
+By The Coding Train
---
+++ express-app/hello-express/index.js
@@ -0,0 +1,22 @@
+// https://www.youtube.com/watch?v=wxbQP1LMZsw&list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X&index=9
+// By The Coding Train
+
+// Express 4.18.2: Fast, unopinionated, minimalist web framework for Node.js
+// Setting up a web server: this file has the express server code
+
+// to have access to the node express web server package, use 'require'
+const express = require('express');
+
+// Creates an Express application: use the whole express function libary for the app
+// The express() function is a top-level function exported by the express module.
+const app = express();
+
+// listen at a port: the server is an application that listens for requests
+// http://localhost:3000/
+app.listen(3000, () => console.log('listening at 3000'));
+
+// if this line of code is not there, then "Cannot GET /" shows up at port 3000
+// therefore, to serve web pages, use express to host static files
+// create a 'public' folder in the main package (data-selfie-app-express)
+// and put the file 'index.html' in the 'public' folder
+app.use(express.static('public'));
